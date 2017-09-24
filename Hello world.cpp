a51342a69db47532090c80ade0d5b756e7832700
HavilandTuff <31802474+HavilandTuff@users.noreply.github.com>
2017-09-24 22:59:13 +0300

    Update Hello world.cpp

--- Hello world.cpp
+++ Hello world.cpp
@@ -5,5 +5,8 @@ using namespace std;
 int main()
 {
   cout<<"Hello world!"<<endl;
+  cout<<"Finaly I see some code"<<endl;
+  cout<<"Now I must learn to execute it!"<<endl;
+  cout<<"See ya then!"<<endl;
   return 0;
 }
